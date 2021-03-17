c1249f7cc59b16688b2b553eeedefc421d56a2fa
Nic <nicholasbshepard@github.ccs.neu.edu>
2021-03-16 21:07:10 -0400

    App writes to RTDB

--- app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/MainActivity.java
+++ app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/MainActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+//    private static final String SERVER_KEY = "AAAAk1Z2RoU:APA91bHx5coormb5Qv_SeCRUTUpcV9Dz1jAt7HY7pV1gMy8kWdpdRfYDNWy1P_mHVOh6jV11iftmrmaFQHFT2amr0eOIC1VYu2kXtTTml5P78c2LNWGB3GZBSYluqZ_f1gZSKRhsKCTt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
