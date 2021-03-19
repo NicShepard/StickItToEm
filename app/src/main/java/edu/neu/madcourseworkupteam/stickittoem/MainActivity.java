79286b3e6ab3f8690cc1d87aecd334ac551ff14e
nberg002 <berg.n@northeastern.edu>
2021-03-19 12:44:13 -0400

    Nicole/Maham pair programming login screen

--- app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/MainActivity.java
+++ app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/MainActivity.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String SERVER_KEY = "AAAAk1Z2RoU:APA91bHx5coormb5Qv_SeCRUTUpcV9Dz1jAt7HY7pV1gMy8kWdpdRfYDNWy1P_mHVOh6jV11iftmrmaFQHFT2amr0eOIC1VYu2kXtTTml5P78c2LNWGB3GZBSYluqZ_f1gZSKRhsKCTt";
 
-    Button go_button;
+    private EditText username;
+    private Button go_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +21,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         go_button = (Button) findViewById(R.id.RTDB);
+        username = (EditText) findViewById(R.id.UserName);
 
         go_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // TODO: Figure out how to carryover entered username to the RTDB activity
                 Intent intent = new Intent(getApplicationContext(), RealTimeDatabaseActivity.class);
                 startActivity(intent);
             }
