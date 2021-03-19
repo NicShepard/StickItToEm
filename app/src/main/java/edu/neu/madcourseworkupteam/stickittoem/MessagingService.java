613ade29cc9db62a2c5cbd1e9eab7240aeeaafde
Nic <nicholasbshepard@github.ccs.neu.edu>
2021-03-19 13:15:16 -0400

    Adding logging code from documentation to messaging service

---
+++ app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/MessagingService.java
@@ -0,0 +1,29 @@
+package edu.neu.madcourseworkupteam.stickittoem;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessagingService extends FirebaseMessagingService {
+
+    //https://firebase.google.com/docs/cloud-messaging/android/first-message
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        final String TAG = "FIREBASE";
+
+        // TODO(developer): Handle FCM messages here.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+    }
+
+}
