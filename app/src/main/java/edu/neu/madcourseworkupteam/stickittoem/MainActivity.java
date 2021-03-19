426264563d5bb6ba8b2d97ca717c7beadd7f86bb
Nic <nicholasbshepard@github.ccs.neu.edu>
2021-03-19 13:33:33 -0400

    Messages logged

--- app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/MainActivity.java
+++ app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/MainActivity.java
@@ -20,10 +20,14 @@ public class MainActivity extends AppCompatActivity {
 //    private static final String SERVER_KEY = "AAAAk1Z2RoU:APA91bHx5coormb5Qv_SeCRUTUpcV9Dz1jAt7HY7pV1gMy8kWdpdRfYDNWy1P_mHVOh6jV11iftmrmaFQHFT2amr0eOIC1VYu2kXtTTml5P78c2LNWGB3GZBSYluqZ_f1gZSKRhsKCTt";
 
     String instanceId;
+    MessagingService messagingService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.messagingService = new MessagingService();
+
         FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(MainActivity.this, new OnSuccessListener<InstanceIdResult>() {
             @Override
             public void onSuccess(InstanceIdResult instanceIdResult) {
--- app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/MessagingService.java
+++ app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/MessagingService.java
@@ -1,13 +1,15 @@
 package edu.neu.madcourseworkupteam.stickittoem;
 
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class MessagingService extends FirebaseMessagingService {
 
-    //https://firebase.google.com/docs/cloud-messaging/android/first-message
+
+//    https://firebase.google.com/docs/cloud-messaging/android/first-message
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         final String TAG = "FIREBASE";
