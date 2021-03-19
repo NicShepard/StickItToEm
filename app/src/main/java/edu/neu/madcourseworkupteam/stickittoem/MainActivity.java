2e4f4415e56bb78aeafc0c034c054fabcb719adb
nberg002 <berg.n@northeastern.edu>
2021-03-19 12:07:03 -0400

    Nicole/Maham pair programming Realtime Database setup

--- app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/MainActivity.java
+++ app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/MainActivity.java
@@ -11,11 +11,21 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String SERVER_KEY = "AAAAk1Z2RoU:APA91bHx5coormb5Qv_SeCRUTUpcV9Dz1jAt7HY7pV1gMy8kWdpdRfYDNWy1P_mHVOh6jV11iftmrmaFQHFT2amr0eOIC1VYu2kXtTTml5P78c2LNWGB3GZBSYluqZ_f1gZSKRhsKCTt";
 
+    Button go_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        go_button = (Button) findViewById(R.id.RTDB);
+
+        go_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RealTimeDatabaseActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
--- app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/User.java
+++ app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/User.java
@@ -3,6 +3,22 @@ package edu.neu.madcourseworkupteam.stickittoem;
 public class User {
 
     public String username;
-    // sent
-    // received
+    public String score;
+    public String datePlayed;
+
+    public User(){
+        // Default constructor
+    }
+
+    public User(String username, String score){
+        this.username = username;
+        this.score = score;
+        this.datePlayed = "2021-03-19";
+    }
+
+    public User(String username, String score, String date) {
+        this.username = username;
+        this.score = score;
+        this.datePlayed = date;
+    }
 }
