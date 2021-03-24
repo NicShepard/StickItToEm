4afbd5b8b7d925e018a4e40968389cc23baf3f7c
Nic <nicholasbshepard@github.ccs.neu.edu>
2021-03-23 21:21:38 -0400

    Maham and Nics notification code revisions, notification sounding

--- app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/MessagingService.java
+++ app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/MessagingService.java
@@ -1,18 +1,18 @@
 package edu.neu.madcourseworkupteam.stickittoem;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.core.Tag;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -26,53 +26,105 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Scanner;
 
-import javax.net.ssl.KeyManager;
-
+/**
+ * Code for this feature was adapted from class code.
+ */
 public class MessagingService extends FirebaseMessagingService {
 
-    private static final String SERVER_KEY = "AAAAk1Z2RoU:APA91bHx5coormb5Qv_SeCRUTUpcV9Dz1jAt7HY7pV1gMy8kWdpdRfYDNWy1P_mHVOh6jV11iftmrmaFQHFT2amr0eOIC1VYu2kXtTTml5P78c2LNWGB3GZBSYluqZ_f1gZSKRhsKCTt";
-
+    private static final String SERVER_KEY =
+            "key=AAAAk1Z2RoU:APA91bHx5coormb5Qv_SeCRUTUpcV9Dz1jAt7HY7pV1gMy8kWdpdRfYDNWy1P_mHVOh6jV11iftmrmaFQHFT2amr0eOIC1VYu2kXtTTml5P78c2LNWGB3GZBSYluqZ_f1gZSKRhsKCTt";
+    private final String TAG = "FIREBASE";
+    private static final String CHANNEL_ID  = "CHANNEL_ID";
+    private static final String CHANNEL_NAME  = "CHANNEL_NAME";
+    private static final String CHANNEL_DESCRIPTION  = "CHANNEL_DESCRIPTION";
     //    https://firebase.google.com/docs/cloud-messaging/android/first-message
+    @Override
+    public void onNewToken(String newToken) {
+        super.onNewToken(newToken);
+        Log.d(TAG, "Refreshed token: " + newToken);
+    }
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         final String TAG = "FIREBASE";
-
-        // TODO(developer): Handle FCM messages here.
         Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        if (remoteMessage.getNotification() != null) {
+            showNotification(remoteMessage);
+        }
+        extractPayloadDataForegroundCase(remoteMessage);
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
             Log.d(TAG, "Message data payload: " + remoteMessage.getData());
         }
-
-        // Check if message contains a notification payload.
+//
+//        // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
     }
 
+    private void showNotification(RemoteMessage remoteMessage) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Notification notification;
+        NotificationCompat.Builder builder;
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
+        {
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,CHANNEL_NAME,NotificationManager.IMPORTANCE_HIGH);
+            // Configure the notification channel
+            notificationChannel.setDescription(CHANNEL_DESCRIPTION);
+            notificationManager.createNotificationChannel(notificationChannel);
+            builder = new NotificationCompat.Builder(this,CHANNEL_ID);
+
+        }
+        else {
+            builder = new NotificationCompat.Builder(this);
+        }
+        notification = builder.setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notificationManager.notify(0,notification);
+
+    }
+
+    public void postToastMessage(final String message) {
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        handler.post(new Runnable() {
+
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void  extractPayloadDataForegroundCase(RemoteMessage remoteMessage) {
+
+        if (remoteMessage.getData() != null) {
+            postToastMessage(remoteMessage.getData().get("title"));
+        }
+    }
 
     /**
-     * Retrieve the device key for a given username using RTDb
+     * While this is supposed to retrieve the device key for a given username using RTDB, here we have it hardcoded.
      * @param username the username of the key we are looking for
      * @return the key as a string
      */
-    private String getKeyForUsername(String username){
-        String token = "";
-
-        return token;
-    }
-
-    @Override
-    public void onNewToken(String newToken) {
-        super.onNewToken(newToken);
-
-        Log.d("Refreshed token: ", newToken);
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
-        // sendRegistrationToServer(refreshedToken);
+    private String getKeyForUsername (String username){
+        return "key=dF3ALcxf2Cs:APA91bGhdHa2HBZVJmJSANUW-2CbMiacFEnL7qNK3X-QM3qPIM2jB_klqGxzpFKadNfT0ZfdeP2XncGc66D3YNuNDiLrldSCSRdCswQ_YNwAgdjlI9McWK214-GRGxCagGI-JaUJgtII";
     }
 
     /**
@@ -82,8 +134,8 @@ public class MessagingService extends FirebaseMessagingService {
      * @param receiver username of the receiver of the message
      * @param content content of the message, in this case an emoji
      */
-    public void messageUsername(View view, String sender, String receiver, String content) {
-        
+    public void messageUsername (View view, String sender, String receiver, String content){
+
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -101,11 +153,12 @@ public class MessagingService extends FirebaseMessagingService {
      * @param receiver username of the receiver of the message
      * @param content content of the message, in this case an emoji
      */
-    private void messageUsernameHelper(View view, String sender, String receiver, String content) {
+    private void messageUsernameHelper (View view, String sender, String receiver, String
+            content){
 
         //Get receiver key
         String receiverDeviceKey = getKeyForUsername(receiver);
-        
+
         //Build json objects
         JSONObject payload = new JSONObject();
         JSONObject notification = new JSONObject();
@@ -118,14 +171,14 @@ public class MessagingService extends FirebaseMessagingService {
             notification.put("badge", "1");
 
             //Build data
-            data.put("title","New Message From " + sender + "!");
-            data.put("content",content);
+            data.put("title", "New Message From " + sender + "!");
+            data.put("content", content);
 
             //Build payload
             payload.put("to", receiverDeviceKey);
             payload.put("priority", "high");
             payload.put("notification", notification);
-            payload.put("data",data);
+            payload.put("data", data);
 
             //Send payload
             URL url = new URL("https://fcm.googleapis.com/fcm/send");
@@ -147,7 +200,7 @@ public class MessagingService extends FirebaseMessagingService {
                 @Override
                 public void run() {
                     Log.e("FIREBASE", "run: " + resp);
-                    Toast.makeText(view.getContext() ,resp,Toast.LENGTH_LONG).show();
+                    Toast.makeText(view.getContext(), resp, Toast.LENGTH_LONG).show();
                 }
             });
         } catch (JSONException | IOException e) {
@@ -160,8 +213,9 @@ public class MessagingService extends FirebaseMessagingService {
      * @param is
      * @return
      */
-    private String convertStreamToString(InputStream is) {
+    private String convertStreamToString (InputStream is){
         Scanner s = new Scanner(is).useDelimiter("\\A");
         return s.hasNext() ? s.next().replace(",", ",\n") : "";
     }
 }
+
