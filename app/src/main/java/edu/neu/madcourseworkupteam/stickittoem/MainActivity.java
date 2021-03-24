2e7ee5e62b22598698cdded602e0e45d272ae21b
Nic <nicholasbshepard@github.ccs.neu.edu>
2021-03-23 20:36:08 -0400

    Main activity loads without crashing

--- app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/MainActivity.java
+++ app/src/main/java/edu/neu/madcourseworkupteam/stickittoem/MainActivity.java
@@ -28,21 +28,37 @@ import android.widget.EditText;
  */
 public class MainActivity extends AppCompatActivity {
 
+    String token;
     private Button go_button;
-    TextView currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Get views
-        go_button = (Button) findViewById(R.id.RTDB);
-        currentUser = findViewById(R.id.UserName);
-    }
 
-    public void goToRTDBActivity(View view) {
-        //Carry the name of the user into the second screen
-        Intent intent = new Intent(getApplicationContext(), RealTimeDatabaseActivity.class);
-        intent.putExtra("CURRENT_USER", currentUser.getText().toString());
-        startActivity(intent);
+        //Get the token and log it for debugging
+        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(MainActivity.this, new OnSuccessListener<InstanceIdResult>() {
+            @Override
+            public void onSuccess(InstanceIdResult instanceIdResult) {
+                setContentView(R.layout.activity_main);
+
+                //Get token
+                token = instanceIdResult.getToken();
+                Log.d("Token", token);
+
+                //Get views
+                go_button = (Button) findViewById(R.id.RTDB);
+                TextView currentUser = findViewById(R.id.UserName);
+
+                //Carry the name of the user into the second screen
+                go_button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(getApplicationContext(), RealTimeDatabaseActivity.class);
+                        intent.putExtra("CURRENT_USER", currentUser.getText().toString());
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
     }
 }
